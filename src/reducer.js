caaa2b4cb66904efa31354fb21f6edc89616bd20
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-27 14:39:47 -0700

    Handled edge case when decreasing amount of cart item to 0

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+import CartItem from './CartItem';
+
 const reducer = (state, action) => {
 	if (action.type === 'CLEAR_CART') {
 		return { ...state, cart: [] };
@@ -15,12 +17,14 @@ const reducer = (state, action) => {
 		return { ...state, cart: newCart };
 	}
 	if (action.type === 'DECREMENT_COUNT') {
-		let newCart = state.cart.map((item) => {
-			if (item.id === action.payload) {
-				return { ...item, amount: item.amount - 1 };
-			}
-			return item;
-		});
+		let newCart = state.cart
+			.map((item) => {
+				if (item.id === action.payload) {
+					return { ...item, amount: item.amount - 1 };
+				}
+				return item;
+			})
+			.filter((item) => item.amount !== 0);
 		return { ...state, cart: newCart };
 	}
 	return state;
